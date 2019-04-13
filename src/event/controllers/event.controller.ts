4c8087afbe2b945b355666d5fd77864807b7e317
Luiz Pedro Franciscatto Guerra <luizpedrofranciscattoguerra@Luiz-Pedro-F-Guerra.local>
2019-04-12 22:34:22 -0300

    wip: listar detalhes do evento

--- src/event/controllers/event.controller.ts
+++ src/event/controllers/event.controller.ts
@@ -2,6 +2,7 @@ import { Model } from 'mongoose';
 import { EventService } from './../services/event.service';
 import { EventModel } from './../models/event.model';
 import { Get, Controller, Post, Body, Res } from '@nestjs/common';
+import { EventSchema } from '../schema/event.Schema';
 
 @Controller('event')
 export class EventController {
@@ -10,8 +11,12 @@ export class EventController {
     @Post()
     async create(@Body() model: EventModel, @Res() res) {
         try {
-            const event = await this.service.create(model);
-            return res.status(200).json(event);
+            // const event = await this.service.create(model);
+            var random = require('random-object-generator');
+            var nn = random.randomObject( new EventModel() );
+            console.log(random);
+            console.log(nn);
+            return res.status(200).json( nn );
         } catch (e) {
             return res.status(500).json(e);
         }
@@ -21,6 +26,7 @@ export class EventController {
     async get(@Res() res): Promise<EventModel[]> {
         try {
             const events = await this.service.get();
+            // console.log(JSON.stringify(events));
             return res.status(200).json(events);
         } catch (e) {
             return res.status(500).json(e);
