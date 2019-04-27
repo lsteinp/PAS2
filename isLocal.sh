d22134163db6731c8fccf3ddb6d6a31e5bfbf948
Cassio Trindade <cassio.trindade@pucrs.br>
2019-04-26 22:25:21 -0300

    alteracoes no ic

--- isLocal.sh
+++ isLocal.sh
@@ -3,13 +3,13 @@
 rm -rf /opt/docker/hubble/api
 rm -rf /opt/docker/hubble/web
 git clone --depth 1 --branch dev http://www.tools.ages.pucrs.br/Hublle/API.git /opt/docker/hubble/api
-git clone --depth 1 --branch dev http://www.tools.ages.pucrs.br/Hublle/Front-angular.git /opt/docker/hubble/web
+git clone --depth 1 --branch develop http://www.tools.ages.pucrs.br/Hublle/Front-angular.git /opt/docker/hubble/web
 nvm use 10.15.3
 cd /opt/docker/hubble/web
 npm install
 ng build
 
-mv -f /opt/docker/hubble/web/dist /opt/docker/hubble/api/
+mv -f /opt/docker/hubble/web/build /opt/docker/hubble/api/web/build
 
 cd /opt/docker/hubble/api
 
