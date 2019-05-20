1bce241efe16de33c687bda41a956ec8c6a82144
Lucas Stein <lucas.cerqueira@acad.pucrs.br>
2019-05-20 19:55:41 -0300

    Query para buscar eventos do usuario

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,7 +1,7 @@
 import { UserModel } from './models/user.model';
 import { InjectModel } from '@nestjs/mongoose';
 import { Injectable, Body, Res } from '@nestjs/common';
-import { Model } from 'mongoose';
+import { Model, Types } from 'mongoose';
 
 @Injectable()
 export class UserService {
@@ -31,4 +31,62 @@ export class UserService {
     async findOneByEmail(email: string): Promise<UserModel> {
         return await this.model.findOne({email: email}).exec()
     }
+
+    async findUserCreatedEvents(id: string, type: string){
+        var query =  await this.model.aggregate(
+          [
+            {
+              '$match': {
+                '_id': Types.ObjectId(id)
+              }
+            }, {
+              '$unwind': {
+                'path': '$'+type, 
+                'preserveNullAndEmptyArrays': false
+              }
+            }, {
+              '$lookup': {
+                'from': 'events', 
+                'localField': type, 
+                'foreignField': '_id', 
+                'as': 'events_doc'
+              }
+            }, {
+              '$project': {
+                '_id': 0, 
+                'events': {
+                  '$arrayElemAt': [
+                    '$events_doc', 0
+                  ]
+                }
+              }
+            }, {
+              '$project': {
+                'events.createdAt': 0, 
+                'events.updatedAt': 0, 
+                'events.__v': 0
+              }
+            }, {
+              '$group': {
+                '_id': {
+                  'id': '$id', 
+                  'name': '$name'
+                }, 
+                type: {
+                  '$addToSet': '$events'
+                }
+              }
+            }, {
+              '$project': {
+                '_id': 0, 
+                type: 1
+              }
+            }
+          ]
+        ) 
+    if(query[0] == undefined){
+        return [];
+    }
+      return query[0].type;        
+    } 
 }
