09d2e4b677906063abc406bd923e4cca17fbc371
Lucas Stein <lucas.cerqueira@acad.pucrs.br>
2019-06-08 12:47:34 -0300

    query em getEventById

--- src/event/services/event.service.ts
+++ src/event/services/event.service.ts
@@ -5,7 +5,7 @@ import { CategoryModel } from './../models/category.model';
 import { EventModel } from './../models/event.model';
 import { InjectModel } from '@nestjs/mongoose';
 import { Injectable, Body, Res } from '@nestjs/common';
-import { Model } from 'mongoose';
+import { Model, Types } from 'mongoose';
 
 @Injectable()
 export class EventService {
@@ -25,8 +25,121 @@ export class EventService {
             return this.model.findOneAndUpdate({_id: id}, model, {new: true}).exec();
     }
 
-    async getEventDetail(id: string): Promise<EventModel> {
-        return await this.model.findOne({_id: id}).exec()
+    async getEventDetail(id: string) {
+        var query = await this.model.aggregate(
+        [
+            {
+              '$match': {
+                '_id': Types.ObjectId(id)
+              }
+            }, {
+              '$unwind': {
+                'path': '$tag', 
+                'preserveNullAndEmptyArrays': false
+              }
+            }, {
+              '$lookup': {
+                'from': 'tags', 
+                'localField': 'tag', 
+                'foreignField': '_id', 
+                'as': 'tags_doc'
+              }
+            }, {
+              '$lookup': {
+                'from': 'users', 
+                'localField': 'createdBy', 
+                'foreignField': '_id', 
+                'as': 'user_doc'
+              }
+            }, {
+              '$lookup': {
+                'from': 'users', 
+                'localField': 'approvedBy', 
+                'foreignField': '_id', 
+                'as': 'app_doc'
+              }
+            }, {
+              '$group': {
+                '_id': '$_id', 
+                'status': {
+                  '$first': '$status'
+                }, 
+                'tag': {
+                  '$addToSet': {
+                    '$arrayElemAt': [
+                      '$tags_doc', 0
+                    ]
+                  }
+                }, 
+                'title': {
+                  '$first': '$title'
+                }, 
+                'description': {
+                  '$first': '$description'
+                }, 
+                'startDate': {
+                  '$first': '$startDate'
+                }, 
+                'startHour': {
+                  '$first': '$startHour'
+                }, 
+                'endHour': {
+                  '$first': '$endHour'
+                }, 
+                'endDate': {
+                  '$first': '$endDate'
+                }, 
+                'price': {
+                  '$first': '$price'
+                }, 
+                'hours': {
+                  '$first': '$hours'
+                }, 
+                'createdBy': {
+                  '$first': {
+                    '$arrayElemAt': [
+                      '$user_doc', 0
+                    ]
+                  }
+                }, 
+                'approvedBy': {
+                  '$first': {
+                    '$arrayElemAt': [
+                      '$app_doc', 0
+                    ]
+                  }
+                }, 
+                'address': {
+                  '$first': '$address'
+                }
+              }
+            }, {
+              '$project': {
+                'tag.createdAt': 0, 
+                'tag.updatedAt': 0, 
+                'tag.__v': 0, 
+                'createdBy.createdAt': 0, 
+                'createdBy.updatedAt': 0, 
+                'createdBy.__v': 0, 
+                'createdBy.password': 0, 
+                'createdBy.email': 0, 
+                'createdBy.interestCategories': 0, 
+                'createdBy.favoritedEvents': 0, 
+                'createdBy.participatedEvents': 0, 
+                'createdBy.createdEvents': 0, 
+                'approvedBy.createdAt': 0, 
+                'approvedBy.updatedAt': 0, 
+                'approvedBy.__v': 0, 
+                'approvedBy.password': 0, 
+                'approvedBy.email': 0, 
+                'approvedBy.interestCategories': 0, 
+                'approvedBy.favoritedEvents': 0, 
+                'approvedBy.participatedEvents': 0, 
+                'approvedBy.createdEvents': 0
+              }
+            }
+          ])
+          return query[0];
     }
 
     async getEventByStatus(status: string): Promise<EventModel> {
