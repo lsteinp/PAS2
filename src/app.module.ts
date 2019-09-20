7a7e6ef7ed44e0a51bdb69c713fdfdefeec72e0c
Lucas Stein <42624148+lsteinp@users.noreply.github.com>
2019-09-20 16:11:27 -0300

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import {MongooseModule} from '@nestjs/mongoose';
-import { EventModule } from './event/event.module';
+import { GroupModule } from 'src/group/group.module';
 import Environment  from './enviroments';
 import { UserModule } from 'src/user/user.module';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [MongooseModule.forRoot(Environment.api_url), UserModule, EventModule, AuthModule],
+  imports: [MongooseModule.forRoot(Environment.api_url), UserModule, GroupModule, AuthModule],
 })
 export class AppModule {}
