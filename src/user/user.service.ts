0648e8956437047ce565e1a9a91edb35e38ef9b2
Lucas Stein <42624148+lsteinp@users.noreply.github.com>
2019-09-20 16:12:25 -0300

    Update user.service.ts

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,3 @@
-import { EventModel } from './../event/models/event.model';
 import { UserModel } from './models/user.model';
 import { InjectModel } from '@nestjs/mongoose';
 import { Injectable, Body, Res } from '@nestjs/common';
@@ -33,4 +32,129 @@ export class UserService {
     user.role = "Avaliador";
     return await this.model.findOneAndUpdate({ email: email }, user, { new: true }).exec()
   }
+
+  async entrarGrupo(idUser: string, idGroup: string): Promise<UserModel> {
+    var user = await this.findOneById(idUser);
+    const convertido = Types.ObjectId(idGroup);
+    user.group = convertido;
+
+    await this.model.findOneAndUpdate({ _id: idUser }, user).exec();
+    return user;
+  }
+
+  async sairGrupo(idUser: string): Promise<UserModel> {
+    var user = await this.findOneById(idUser);
+    user.group = null;
+
+    await this.model.findOneAndUpdate({ _id: idUser }, user).exec();
+    return user;
+  }
+
+  async getUserDetail(id: string) {
+    var query = [];
+    if(id){
+        query.push('');
+    }
+    query.push({
+          '$match': {
+              '_id': Types.ObjectId('5d84f171ab22f209f8e314f0')
+          }
+      }, {
+          '$lookup': {
+              'from': 'groups', 
+              'localField': 'group', 
+              'foreignField': '_id', 
+              'as': 'group'
+          }
+      }, {
+          '$addFields': {
+              'group': {
+                  '$arrayElemAt': [
+                      '$group', 0
+                  ]
+              }
+          }
+      }, {
+          '$unwind': {
+              'path': '$group.members', 
+              'preserveNullAndEmptyArrays': true
+          }
+      }, {
+          '$lookup': {
+              'from': 'users', 
+              'localField': 'group.members', 
+              'foreignField': '_id', 
+              'as': 'members_doc'
+          }
+      }, {
+          '$addFields': {
+              'members_doc': {
+                  '$arrayElemAt': [
+                      '$members_doc', 0
+                  ]
+              }
+          }
+      }, {
+          '$project': {
+              'password': 0, 
+              'createdAt': 0, 
+              'updatedAt': 0, 
+              '__v': 0, 
+              'group.members': 0, 
+              'group.createdAt': 0, 
+              'group.updatedAt': 0, 
+              'group.__v': 0, 
+              'group.createdBy': 0, 
+              'group.evaluation.evaluator': 0, 
+              'group.evaluation._id': 0, 
+              'members_doc.password': 0, 
+              'members_doc.group': 0, 
+              'members_doc.createdAt': 0, 
+              'members_doc.updatedAt': 0, 
+              'members_doc.__v': 0
+          }
+      }, {
+          '$group': {
+              '_id': '$_id', 
+              'role': {
+                  '$first': '$role'
+              }, 
+              'firstName': {
+                  '$first': '$firstName'
+              }, 
+              'lastName': {
+                  '$first': '$lastName'
+              }, 
+              'email': {
+                  '$first': '$email'
+              }, 
+              'group': {
+                  '$first': '$group'
+              }, 
+              'members': {
+                  '$addToSet': '$members_doc'
+              }
+          }
+      }, {
+          '$addFields': {
+              'group.members': '$members'
+          }
+      }, {
+          '$project': {
+              'members': 0
+          }
+      }
+    )
+    if (id) {
+        query[0] = {
+            ...{
+                '$match': {
+                    '_id': Types.ObjectId(id)
+                }
+            }
+        };
+    }
+    return await this.model.aggregate([query]);
+}
+
 }
